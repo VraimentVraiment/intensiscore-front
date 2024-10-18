187626b7cf7c89d8c26f78e023a4980f1631e138
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-10-18 15:43:12 +0200

    Improve report listing

---
+++ src/components/report/ReportCard.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+defineProps<{
+  number: number
+  date: string
+  surveyLabel?: string
+}>()
+
+defineEmits(['click'])
+</script>
+
+<template>
+  <div
+    class="report-card"
+  >
+    <template v-if="surveyLabel">
+      <p class="text-xs mb-0">
+        Diagnostic <b>n°{{ number }}</b>
+      </p>
+      <p class="text-lg mt-0 mb-2 font-bold">
+        {{ surveyLabel }}
+      </p>
+    </template>
+    <template v-else>
+      <p class="text-lg mb-2">
+        Diagnostic <b>n°{{ number }}</b>
+      </p>
+    </template>
+    <p class="mt-1 text-sm text-gray-500">
+      Le {{ date }}
+    </p>
+    <div class="flex self-stretch align-end">
+      <PcoButton
+        class="w-full"
+        size="sm"
+        secondary
+        @click="$emit('click')"
+      >
+        Consulter le résultat
+      </PcoButton>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.report-card {
+  @apply p-4 bg-bg-default;
+
+  border-radius: $border-radius--md;
+  border: solid 1px $border--default;
+  box-shadow: $shadow--md;
+}
+</style>
--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
   const config = useRuntimeConfig()
-  if (!config.isAdmin) {
+  if (import.meta.server && !config.isAdmin) {
     return navigateTo('/')
   }
 })
