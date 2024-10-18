570632e596412403cdb9a89ed21d66107c90211e
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-10-18 16:02:11 +0200

    Manually add favicon.ico

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+const {
+  siteTitle,
+  titleSeparator,
+} = useAppConfig()
+
 useHead({
+  titleTemplate: (pageTitle) => {
+    return [
+      pageTitle,
+      siteTitle,
+    ]
+      .filter(Boolean)
+      .join(` ${titleSeparator} `)
+  },
   meta: [
     {
       name: 'google-site-verification',
       content: 'aQGrgOfwOtCy5IiKcgYzEv8kChIC-r4mordTsbAiDbo',
+    },
+
+  ],
+  link: [
+    /**
+     * Nuxt seo plugin should automatically add the favicon tags to the head, but if it doesn't...
+     * @see https://nuxtseo.com/experiments/guides/app-icons#icon
+     */
+    {
+      rel: 'icon',
+      href: '/favicon.ico',
+      sizes: 'any',
     },
   ],
 })
